712046696165a560be4bc36379b5f68d1a1cbe97
autumn <qy814379857@foxmail.com>
2018-11-13 13:16:13 +0800

    手写AOP

--- src/main/java/com/autumn/aop/ProxyChain.java
+++ src/main/java/com/autumn/aop/ProxyChain.java
@@ -10,14 +10,14 @@ import java.util.List;
  * 代理链
  */
 public class ProxyChain {
-    private final Class<?> targetClass;
-    private final Object targetObject;
-    private final Method targetMethod;
-    private final MethodProxy methodProxy;
-    private final Object[] methodParams;
+    private final Class<?> targetClass;    //代理类
+    private final Object targetObject;    //目标对象
+    private final Method targetMethod;    //目标方法
+    private final MethodProxy methodProxy;    //方法代理
+    private final Object[] methodParams;    //方法参数
 
-    private List<Proxy> proxyList = new ArrayList<Proxy>();
-    private int proxyIndex = 0;
+    private List<Proxy> proxyList = new ArrayList<Proxy>();   //代理列表
+    private int proxyIndex = 0;    //代理索引
 
     public ProxyChain(Class<?> targetClass, Object targetObject, Method targetMethod, MethodProxy methodProxy, Object[] methodParams, List<Proxy> proxyList) {
         this.targetClass = targetClass;
@@ -42,10 +42,11 @@ public class ProxyChain {
 
     public Object doProxyChain() throws Throwable{
         Object methodResult;
-        if (proxyIndex<proxyList.size()){
+        if (proxyIndex<proxyList.size()){   //如果代理索引小于代理列表大小
+            //从列表中取出相应的Proxy对象，调用器doProxy方法
             methodResult = proxyList.get(proxyIndex++).doProxy(this);
-        }else {
-            methodResult = methodProxy.invokeSuper(targetObject,methodParams);
+        }else {    //所有代理遍历完后
+            methodResult = methodProxy.invokeSuper(targetObject,methodParams);  //执行目标对象业务
         }
         return methodResult;
     }
