d9d1d8518cab270eb8e453e489cd6d9763128735
autumn <qy814379857@foxmail.com>
2018-11-28 12:55:46 +0800

    ThreadLocal改进DBUtil

--- src/main/java/com/autumn/threadlocalcase/DBUtil.java
+++ src/main/java/com/autumn/threadlocalcase/DBUtil.java
@@ -20,25 +20,33 @@ public class DBUtil {
     private static final Logger LOGGER = LoggerFactory.getLogger(DBUtil.class);
     //数据库配置
     private static final String DRIVER = "com.mysql.jdbc.Driver";
-    private static final String URL = "jdbc:mysql://222.222.221.198:3306/demo2";
+    private static final String URL = "jdbc:mysql://www.52zt.online:3306/demo2";
     private static final String USERNAME = "root";
     private static final String PASSWORD = "autumn-19950926";
 
     //定义一个数据库连接
-    private static Connection conn = null;
+    //private static Connection conn = null;
+    //定义一个用于放置数据库连接的局部线程变量(是每个线程拥有自己的连接)
+    private static ThreadLocal<Connection> connContainer = new ThreadLocal<Connection>();
 
     /**
      * 获取数据库连接
      * @return
      */
     public static Connection getConnection(){
+        Connection conn = connContainer.get();   //从ThreadLocal中获取conn
+
         try {
-            /*JDBC获取连接*/
-            Class.forName(DRIVER);
-            conn = DriverManager.getConnection(URL,USERNAME,PASSWORD);
+            if (conn == null) {   //从ThreadLocal中拿到的conn如果为null
+                /*JDBC获取连接*/
+                Class.forName(DRIVER);
+                conn = DriverManager.getConnection(URL, USERNAME, PASSWORD);
+            }
         } catch (Exception e) {
             e.printStackTrace();  //在catlina.out中打印
             LOGGER.error("get connection failure",e);
+        }finally {
+            connContainer.set(conn);
         }
         return conn;
     }
@@ -47,12 +55,15 @@ public class DBUtil {
      * 关闭数据库连接
      */
     public static void closeConnection(){
+        Connection conn = connContainer.get();   //从ThreadLocal中获取conn
         if (conn!=null){
             try {
                 conn.close();
             } catch (SQLException e) {
                 e.printStackTrace();
                 LOGGER.error("close connection failure",e);
+            }finally {
+                connContainer.remove();   //从ThreadLocal中删除当前线程的conn
             }
         }
     }
